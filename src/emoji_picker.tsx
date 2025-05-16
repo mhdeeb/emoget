b58d1dc321330336cceed96d1d74d5ff3e355682
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2025-05-17 01:58:49 +0300

    Finish: pre-alpha

--- src/emoji_picker.tsx
+++ src/emoji_picker.tsx
@@ -4,6 +4,8 @@ import "./emoji_picker.css";
 import "@/index.css";
 import { useWebSocketStore } from "./lib/websocket-service";
 import { invoke } from "@tauri-apps/api/core";
+import { Button } from "@/components/ui/button";
+import { ChevronRight, X } from "lucide-react";
 
 export default function EmojiPickerComponent() {
   const { sendEmote, status, receivedCount } = useWebSocketStore();
@@ -28,7 +30,7 @@ export default function EmojiPickerComponent() {
   return (
     <div className="emoji-picker">
       <div data-tauri-drag-region className="drag-region"></div>
-      <div className="w-full flex items-center justify-between mb-2 text-white text-sm md:hidden">
+      <div className="w-full flex items-center justify-between py-2 pr-2 text-white select-none text-sm md:hidden">
         <div className="flex items-center gap-2 px-4 py-2 bg-sidebar-primary/20 rounded-lg">
           <div className="flex items-center gap-1.5">
             <div
@@ -40,20 +42,23 @@ export default function EmojiPickerComponent() {
                   : "bg-red-400"
               }`}
             />
-            <span className="text-xs font-medium text-white">{status}</span>
+            <span className="font-medium flex items-center">{status}</span>
           </div>
           <div className="h-3 w-px bg-white/20" />
-          <div className="text-xs font-medium text-white">
-            Received: {receivedCount}
-          </div>
+          <div className="text-xs font-medium">Received: {receivedCount}</div>
         </div>
-        <button onClick={handleHide}>x</button>
+        <Button
+          onClick={handleHide}
+          variant="ghost"
+          className="hover:bg-red-500/50 hover:text-white">
+          <X />
+        </Button>
       </div>
       <EmojiPicker
         onEmojiClick={handleEmojiClick}
         theme={Theme.DARK}
         width={"100%"}
-        height={500}
+        height={490}
       />
       <Input
         name="emoji"
